50d64c32eb88e214a38f5c2c0ccade22c9de5d3c
Kyle Paulsen <kyle.a.paulsen@gmail.com>
2020-06-06 04:33:16 -0700

    :star: A commit :star:

--- chiptuneSynth/main.js
+++ chiptuneSynth/main.js
@@ -5,6 +5,14 @@ document.querySelectorAll('[data-ui]').forEach(item => {
     ui[item.dataset.ui] = item;
 });
 
+const debounce = (fn, ms) => {
+    let timeout;
+    return () => {
+        clearTimeout(timeout);
+        timeout = setTimeout(fn, ms);
+    };
+};
+
 // removes all delay from playing notes.
 Tone.context.lookAhead = 0;
 
@@ -15,11 +23,12 @@ const masterGain = new Tone.Gain(0.5);
 masterGain.connect(compressor);
 
 const MAX_SYNTHS = 15;
+let allSynths = [];
 let synthPool = [];
 let activeSynths = {};
 let octave = 4;
 
-const setInstrument = instrument => {
+const stopAllNotes = () => {
     Object.values(activeSynths).forEach(synth => {
         if (synth) {
             synth.triggerRelease();
@@ -27,14 +36,21 @@ const setInstrument = instrument => {
         }
     });
     activeSynths = {};
+    ui.currentNotes.textContent = '';
+};
+
+const setInstrument = instrument => {
+    stopAllNotes();
     const nextInstrument = instruments[instrument];
     if (nextInstrument) {
         synthPool.forEach(synth => synth.dispose());
         synthPool = [];
+        allSynths = [];
 
         for (let x = 0; x < MAX_SYNTHS; x++) {
             const synth = new Tone.Synth(nextInstrument).connect(masterGain);
             synthPool.push(synth);
+            allSynths.push(synth);
         }
     }
 };
@@ -95,6 +111,73 @@ const stopNote = note => {
     }
 };
 
+const updateMidiDeviceSelect = () => {
+    ui.midiDeviceSelect.innerHTML = '<option>None</option>';
+    WebMidi.inputs.forEach(input => {
+        const opt = document.createElement('option');
+        opt.value = input.id;
+        opt.textContent = input.name;
+        ui.midiDeviceSelect.appendChild(opt);
+    });
+};
+
+const removeAllListeners = () => {
+    WebMidi.inputs.forEach(input => {
+        input.removeListener('noteon');
+        input.removeListener('noteoff');
+        input.removeListener('pitchbend');
+        input.removeListener('midimessage');
+    });
+};
+
+const addListeners = input => {
+    input.addListener('noteon', 'all', e => {
+        startNote(e.note.name + e.note.octave);
+    });
+
+    input.addListener('noteoff', 'all', e => {
+        stopNote(e.note.name + e.note.octave);
+    });
+
+    input.addListener('pitchbend', 'all', e => {
+        const detune = 1200 * e.value;
+        allSynths.forEach(synth => {
+            synth.detune.value = detune;
+        });
+    });
+
+    input.addListener('midimessage', 'all', e => {
+        const command = e.data[0] >> 4;
+        const channel = (e.data[0] & 0xf) + 1;
+        if (channel !== 9 && command !== 15) {
+            console.log(`${input.name}: Chan: ${channel}, CMD: ${command} : `, e.data);
+        }
+    });
+};
+
+let currentMidiDevice;
+ui.midiDeviceSelect.addEventListener('change', () => {
+    removeAllListeners();
+    stopAllNotes();
+
+    currentMidiDevice = ui.midiDeviceSelect.value;
+    const input = WebMidi.getInputById(currentMidiDevice);
+    if (input) {
+        addListeners(input);
+    }
+});
+
+const connectionChange = debounce(() => {
+    updateMidiDeviceSelect();
+    removeAllListeners();
+    stopAllNotes();
+
+    const input = WebMidi.getInputById(currentMidiDevice);
+    if (input) {
+        addListeners(input);
+    }
+}, 200);
+
 WebMidi.enable(err => {
     if (err) {
         // eslint-disable-next-line no-alert
@@ -103,15 +186,8 @@ WebMidi.enable(err => {
         return;
     }
 
-    WebMidi.inputs.forEach(input => {
-        input.addListener('noteon', 'all', e => {
-            startNote(e.note.name + e.note.octave);
-        });
-
-        input.addListener('noteoff', 'all', e => {
-            stopNote(e.note.name + e.note.octave);
-        });
-    });
+    WebMidi.addListener('connected', connectionChange);
+    WebMidi.addListener('disconnected', connectionChange);
 });
 
 window.addEventListener('keydown', e => {
